efc239b84c36515c302e3e15bb590c7949012527
DunkDaddy <59820523+DunkDaddy@users.noreply.github.com>
2022-05-12 10:29:26 +0200

    Kitchen2.0

---
+++ app/src/main/java/com/example/cafevesuviusapp/Kitchen_Order_Two.java
@@ -0,0 +1,14 @@
+package com.example.cafevesuviusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kitchen_Order_Two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen_order_two);
+    }
+}
