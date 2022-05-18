a35afa40272d435fad25dda4f00365883e282ba8
Undying-Merch <73592997+Undying-Merch@users.noreply.github.com>
2022-05-18 10:22:27 +0200

    Updated Classes

--- app/src/main/java/com/example/cafevesuviusapp/Classes/Chef_Class.java
+++ app/src/main/java/com/example/cafevesuviusapp/Classes/Chef_Class.java
@@ -4,4 +4,13 @@ public class Chef_Class {
 
     public int id;
     public String name;
+
+
+    public void do_Dish(Dish_Class dish){
+
+    }
+
+    public void report_Dish_Done(Dish_Class dish){
+
+    }
 }
--- app/src/main/java/com/example/cafevesuviusapp/Classes/Dish_Class.java
+++ app/src/main/java/com/example/cafevesuviusapp/Classes/Dish_Class.java
@@ -7,12 +7,12 @@ public class Dish_Class {
     public int id;
     public String name;
     public List<Ingridients_Class> dish_Ingredients;
-    public List<Ingridients_Class> dish_Extra_Ingridients;
+    public List<Ingridients_Class> dish_Extra_Ingredients;
 
-    public void addIngredient(Ingridients_Class X){
+    public void addIngredient(Ingridients_Class ingredient){
 
     }
-    public void removeIngredient(Ingridients_Class X){
+    public void removeIngredient(Ingridients_Class ingredient){
 
     }
 }
