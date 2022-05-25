bcbc8c4fe72c10574b0e2023e39f387f0e40a05e
Claus Nordahl Dimon <53813074+clausdimon@users.noreply.github.com>
2022-05-25 13:25:40 +0200

    Menu Sprint Done
    There is now a working activity in menu, where there is a few TODO, that when done, it will be all finished

---
+++ app/src/main/java/com/example/cafevesuviusapp/Classes/MenuAdapter.java
@@ -0,0 +1,41 @@
+package com.example.cafevesuviusapp.Classes;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.cafevesuviusapp.R;
+
+import java.util.List;
+
+public class MenuAdapter  extends ArrayAdapter<MenuItem_Class> {
+    Context context;
+    int resource;
+    List<MenuItem_Class> menuItemsList;
+    public MenuAdapter(Context context, int resource, List<MenuItem_Class> menuItemsList)
+    {
+        super(context, resource, menuItemsList);
+        this.context = context;
+        this.resource = resource;
+        this.menuItemsList = menuItemsList;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(resource, null, false);
+        TextView name = view.findViewById(R.id.Dish_Name);
+        TextView description = view.findViewById(R.id.Dish_Description);
+        TextView price = view.findViewById(R.id.Dish_Price);
+        MenuItem_Class menuItem = menuItemsList.get(position);
+        name.setText(menuItem.name);
+        description.setText(menuItem.description);
+        price.setText(menuItem.price.toString());
+        return view;
+    }
+}
--- app/src/main/java/com/example/cafevesuviusapp/Menu.java
+++ app/src/main/java/com/example/cafevesuviusapp/Menu.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.ListView;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -14,6 +15,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.cafevesuviusapp.Classes.MenuAdapter;
 import com.example.cafevesuviusapp.Classes.MenuItem_Class;
 
 import org.json.JSONArray;
@@ -27,14 +29,20 @@ public class Menu extends AppCompatActivity {
 
     private String server_url = "http://10.0.2.2:8000/menuitems-list/?format=json";
     RequestQueue requestQueue;
-    List<MenuItem_Class> menuList;
-    RelativeLayout appetizers, burger, Sandwich, pasta, salad, drink;
+    List<MenuItem_Class> burgerMenu, appetizerMenu, sandwichMenu, pastaMenu, saladMenu, drinkMenu;
+    MenuAdapter burgerAdapter, appetizersAdapter, sandwichAdapter, pastaAdapter, saladAdapter, drinkAdapter;
+    ListView burger, appetizers, Sandwich, pasta, salad, drink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        menuList = new ArrayList<>();
+        burgerMenu = new ArrayList<>();
+        appetizerMenu = new ArrayList<>();
+        sandwichMenu = new ArrayList<>();
+        pastaMenu = new ArrayList<>();
+        saladMenu = new ArrayList<>();
+        drinkMenu = new ArrayList<>();
         appetizers = findViewById(R.id.Appetizers);
         burger = findViewById(R.id.Burger);
         Sandwich = findViewById(R.id.Sandwich);
@@ -44,8 +52,18 @@ public class Menu extends AppCompatActivity {
 
         requestQueue = Volley.newRequestQueue(this);
         getData();
-
-
+        appetizersAdapter = new MenuAdapter(this, R.layout.menu_item, appetizerMenu);
+        burgerAdapter = new MenuAdapter(this, R.layout.menu_item, burgerMenu);
+        sandwichAdapter = new MenuAdapter(this, R.layout.menu_item, sandwichMenu);
+        pastaAdapter = new MenuAdapter(this, R.layout.menu_item, pastaMenu);
+        saladAdapter = new MenuAdapter(this, R.layout.menu_item, saladMenu);
+        drinkAdapter = new MenuAdapter(this, R.layout.menu_item, drinkMenu);
+        appetizers.setAdapter(appetizersAdapter);
+        burger.setAdapter(burgerAdapter);
+        Sandwich.setAdapter(sandwichAdapter);
+        pasta.setAdapter(pastaAdapter);
+        salad.setAdapter(saladAdapter);
+        drink.setAdapter(drinkAdapter);
     }
     private void getData() {
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, server_url, null, new Response.Listener<JSONArray>() {
@@ -55,39 +73,52 @@ public class Menu extends AppCompatActivity {
                 try {
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        View row = getLayoutInflater().inflate(R.layout.menu_item, null);
-                        switch (jsonObject.getInt("categoryId"))
+                        String name = jsonObject.getString("name");
+                        Double price = jsonObject.getDouble("price");
+                        String description = jsonObject.getString("description");
+                        int id = jsonObject.getInt("id");
+                        int categoryId = jsonObject.getInt("categoryId");
+                        MenuItem_Class item = new MenuItem_Class(id, name, price, description, categoryId);
+                        //TODO: instead of switch use data from category Table, and find the right one that we need to use.
+                        // /category-list/
+                        switch (categoryId)
                         {
+
                             //Burger
                             case 1:
-                                burger.addView(row);
+                                burgerMenu.add(item);
                                 break;
-                            //drink
+                            //Drink
                             case 2:
-                                drink.addView(row);
+                                drinkMenu.add(item);
+                                break;
+                            //Appetizer
+                            case 3:
+                                appetizerMenu.add(item);
+                                break;
+                            //Sandwich
+                            case 4:
+                                sandwichMenu.add(item);
+                                break;
+                            //Pasta
+                            case 5:
+                                pastaMenu.add(item);
+                                break;
+                            //Salad
+                            case 6:
+                                saladMenu.add(item);
                                 break;
                             default:
                                 break;
                         }
-                        String name = jsonObject.getString("name");
-                        Double price = jsonObject.getDouble("price");
-                        String description = jsonObject.getString("description");
 
-                        TextView nameView, descriptionView, priceView;
-
-                        nameView = row.findViewById(R.id.Dish_Name);
-                        nameView.setText(name);
-
-                        descriptionView = row.findViewById(R.id.Dish_Description);
-                        descriptionView.setText(description);
-
-                        priceView = row.findViewById(R.id.Dish_Price);
-                        priceView.setText(price+"");
-
-                        int id = jsonObject.getInt("id");
-                        int categoryId = jsonObject.getInt("categoryId");
-                        menuList.add(new MenuItem_Class(id, name, price, description, categoryId));
                     }
+                    burgerAdapter.notifyDataSetChanged();
+                    drinkAdapter.notifyDataSetChanged();
+                    appetizersAdapter.notifyDataSetChanged();
+                    sandwichAdapter.notifyDataSetChanged();
+                    pastaAdapter.notifyDataSetChanged();
+                    saladAdapter.notifyDataSetChanged();
                 } catch (Exception w) {
                     Toast.makeText(Menu.this, w.getMessage(), Toast.LENGTH_LONG).show();
                 }
