99649b1b214cef448ef4236e979a14c2cf805449
Undying-Merch <jonasnikolailj@gmail.com>
2022-06-03 08:25:48 +0200

    Added GUI for making showing order
    Made the GUI for showing and retrieveing orders, made it clickable so it show the one specific order, but are at a bug where Status object list isn't working and receiving data.

--- app/src/main/java/com/example/cafevesuviusapp/Classes/Status_Class.java
+++ app/src/main/java/com/example/cafevesuviusapp/Classes/Status_Class.java
@@ -3,12 +3,12 @@ package com.example.cafevesuviusapp.Classes;
 public class Status_Class {
 
     public int id;
-    public String status;
+    public String statusName;
 
     public Status_Class(){}
 
     public Status_Class(int id, String status){
-        this.status = status;
         this.id = id;
+        this.statusName = status;
     }
 }
--- app/src/main/java/com/example/cafevesuviusapp/Kitchen_Order_Two.java
+++ app/src/main/java/com/example/cafevesuviusapp/Kitchen_Order_Two.java
@@ -2,6 +2,8 @@ package com.example.cafevesuviusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -41,91 +43,12 @@ public class Kitchen_Order_Two extends AppCompatActivity {
     ArrayList<MenuItem_Class> menuItems;
     ArrayList<Order_Class> orders;
     ArrayList<OrderItems_Class> orderItems;
-    ArrayList<Status_Class> status;
+    ArrayList<Status_Class> statusArray;
     List<String> received, preparing, awaiting, done = new ArrayList<String>();
 
     ArrayAdapter<String> receiveAdapter, workingAdapter, awaitingAdapter;
 
-    public void putIntoMenuitems(int id, String name){
-        MenuItem_Class menuItem = new MenuItem_Class(id, name);
-        menuItems.add(menuItem);
-    }
-    public void putIntoStatus(int id, String name){
-        Status_Class newStatus = new Status_Class(id, name);
-        status.add(newStatus);
-    }
-    public void putIntoOrders(int id, int statusId){
-        Order_Class order = new Order_Class(id, statusId);
-        orders.add(order);
-    }
-    public void putIntoOrderItems(int menuItemId, int orderItemId){
-        OrderItems_Class orderItem = new OrderItems_Class(orderItemId, menuItemId);
-        orderItems.add(orderItem);
-    }
 
-    public void clearStatusLists(){
-        received.clear();
-        preparing.clear();
-        awaiting.clear();
-        done.clear();
-    }
-    public void statusLists(){
-        String fullOrder = "";
-        for (int i = 0; i < orders.size(); i++){
-            if (orders.get(i).status_Id == 1){
-                fullOrder = addItemToOrder(orders.get(i).id);
-                orders.get(i).fullOrder = fullOrder;
-                received.add(fullOrder);
-            }
-            else if(orders.get(i).status_Id == 2){
-                fullOrder = addItemToOrder(orders.get(i).id);
-                orders.get(i).fullOrder = fullOrder;
-                preparing.add(fullOrder);
-            }
-            else if(orders.get(i).status_Id == 3){
-                fullOrder = addItemToOrder(orders.get(i).id);
-                orders.get(i).fullOrder = fullOrder;
-                awaiting.add(fullOrder);
-            }
-            else if(orders.get(i).status_Id == 4){
-                fullOrder = addItemToOrder(orders.get(i).id);
-                orders.get(i).fullOrder = fullOrder;
-                done.add(fullOrder);
-            }
-        }
-    }
-
-
-    public void updateView(){
-        ListView awaitingList = (ListView) findViewById(R.id.awaitingListId);
-        ListView workingList = (ListView) findViewById(R.id.workingListId);
-        ListView receivedList = (ListView) findViewById(R.id.recievedListId);
-        receiveAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, received);
-        workingAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, preparing);
-        awaitingAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, done);
-        awaitingList.setAdapter(awaitingAdapter);
-        workingList.setAdapter(workingAdapter);
-        receivedList.setAdapter(receiveAdapter);
-    }
-
-    public String addItemToOrder(int orderId){
-        String items = String.valueOf(orderId) + ": ";
-        int menuItem = 0;
-        for (int i = 0; i < orderItems.size(); i++){
-            if (orderItems.get(i).order_Id == orderId){
-                menuItem = orderItems.get(i).menuItem_Id;
-                items = items + menuItems.get(menuItem).name + " - ";
-            }
-        }
-        //orders.get(orderId).fullOrder = items;
-        return items;
-
-    }
-
-    public void manualViewUpdate(View view){
-        statusLists();
-        updateView();
-    }
 
 
     @Override
@@ -136,7 +59,7 @@ public class Kitchen_Order_Two extends AppCompatActivity {
         orders = new ArrayList<>();
         orderItems = new ArrayList<>();
         menuItems = new ArrayList<>();
-        status = new ArrayList<>();
+        statusArray = new ArrayList<>();
         received = new ArrayList<>();
         preparing = new ArrayList<>();
         awaiting = new ArrayList<>();
@@ -157,21 +80,25 @@ public class Kitchen_Order_Two extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String order = receivedView.getItemAtPosition(position).toString();
-
+                int orderId = findOrder(order);
+                String currentStatus = statusArray.get(orders.get(orderId).status_Id).statusName;
+                nextStatus(order, orderId, currentStatus);
             }
         });
         doingView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String order = doingView.getItemAtPosition(position).toString();
-
+                int orderId = findOrder(order);
+                nextStatus(order, orderId, statusArray.get(orders.get(orderId).status_Id).statusName);
             }
         });
         awaitingView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String order = doingView.getItemAtPosition(position).toString();
-
+                int orderId = findOrder(order);
+                nextStatus(order, orderId, statusArray.get(orders.get(orderId).status_Id).statusName);
             }
         });
 
@@ -277,6 +204,116 @@ public class Kitchen_Order_Two extends AppCompatActivity {
             }
         });
         requestQueue.add(jsonArrayRequest);
+    }
+
+    public void putIntoMenuitems(int id, String name){
+        MenuItem_Class menuItem = new MenuItem_Class(id, name);
+        menuItems.add(menuItem);
+    }
+    public void putIntoStatus(int id, String name){
+        Status_Class newStatus = new Status_Class(id, name);
+        statusArray.add(newStatus);
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+    }
+    public void putIntoOrders(int id, int statusId){
+        Order_Class order = new Order_Class(id, statusId);
+        orders.add(order);
+    }
+    public void putIntoOrderItems(int menuItemId, int orderItemId){
+        OrderItems_Class orderItem = new OrderItems_Class(orderItemId, menuItemId);
+        orderItems.add(orderItem);
+    }
+
+    public void clearStatusLists(){
+        received.clear();
+        preparing.clear();
+        awaiting.clear();
+        done.clear();
+    }
+    public void statusLists(){
+        String fullOrder = "";
+        for (int i = 0; i < orders.size(); i++){
+            if (orders.get(i).status_Id == 1){
+                fullOrder = addItemToOrder(orders.get(i).id);
+                orders.get(i).fullOrder = fullOrder;
+                received.add(fullOrder);
+            }
+            else if(orders.get(i).status_Id == 2){
+                fullOrder = addItemToOrder(orders.get(i).id);
+                orders.get(i).fullOrder = fullOrder;
+                preparing.add(fullOrder);
+            }
+            else if(orders.get(i).status_Id == 3){
+                fullOrder = addItemToOrder(orders.get(i).id);
+                orders.get(i).fullOrder = fullOrder;
+                awaiting.add(fullOrder);
+            }
+            else if(orders.get(i).status_Id == 4){
+                fullOrder = addItemToOrder(orders.get(i).id);
+                orders.get(i).fullOrder = fullOrder;
+                done.add(fullOrder);
+            }
+        }
+    }
+
+    public void nextStatus(String fullOrder, int orderId, String status){
+        DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(fullOrder).setPositiveButton("Move order to " + status + "?", dialogListener).setNegativeButton("Cancel", dialogListener).show();
+
+    }
+
+    public void updateView(){
+        ListView awaitingList = (ListView) findViewById(R.id.awaitingListId);
+        ListView workingList = (ListView) findViewById(R.id.workingListId);
+        ListView receivedList = (ListView) findViewById(R.id.recievedListId);
+        receiveAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, received);
+        workingAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, preparing);
+        awaitingAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, done);
+        awaitingList.setAdapter(awaitingAdapter);
+        workingList.setAdapter(workingAdapter);
+        receivedList.setAdapter(receiveAdapter);
+    }
+
+    public String addItemToOrder(int orderId){
+        String items = String.valueOf(orderId) + ": ";
+        int menuItem = 0;
+        for (int i = 0; i < orderItems.size(); i++){
+            if (orderItems.get(i).order_Id == orderId){
+                menuItem = orderItems.get(i).menuItem_Id;
+                items = items + menuItems.get(menuItem).name + " - ";
+            }
+        }
+        //orders.get(orderId).fullOrder = items;
+        return items;
+
+    }
+    public int findOrder(String fullOrder){
+        int orderId = 0;
+        for (int i = 0; i < orders.size(); i++){
+            if (orders.get(i).fullOrder.matches(fullOrder)){
+                orderId = orders.get(i).id;
+            }
+        }
+        return orderId;
+    }
+
+    public void manualViewUpdate(View view){
+        statusLists();
+        updateView();
     }
 
 }
