63b392f30af3e5bbf8d4f848175262fae3a1e628
DunkDaddy <59820523+DunkDaddy@users.noreply.github.com>
2022-06-07 13:40:55 +0200

    Completed Login
    Login has been completed ill be it not 100% secure but it has been completed

--- app/src/main/java/com/example/cafevesuviusapp/Classes/Employee_Class.java
+++ app/src/main/java/com/example/cafevesuviusapp/Classes/Employee_Class.java
@@ -1,16 +1,18 @@
 package com.example.cafevesuviusapp.Classes;
 
-public class Login_Class {
+public class Employee_Class {
 
     public int id;
     public String name;
     public String password;
+    public int rankId;
 
 
-    public Login_Class(){}
-    public Login_Class(int id, String name, String password){
+    public Employee_Class(){}
+    public Employee_Class(int id, String name, String password, int rank){
         this.id = id;
         this.name = name;
         this.password = password;
+        this.rankId = rank;
     }
 }
--- app/src/main/java/com/example/cafevesuviusapp/Login.java
+++ app/src/main/java/com/example/cafevesuviusapp/Login.java
@@ -5,39 +5,109 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.cafevesuviusapp.Classes.Location_Class;
-import com.example.cafevesuviusapp.Classes.Login_Class;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.cafevesuviusapp.Classes.Employee_Class;
 import com.example.cafevesuviusapp.Classes.Rank_Class;
-import com.example.cafevesuviusapp.Classes.Tables_Class;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
 
-    String employeeName;
-    String employeePassword;
-    String rankName;
-    int employeeId;
-    int rankId;
-    private String employee_url = "http://5.186.68.226:8000/employee-list/?format=json";
-    private String rank_url = "http://5.186.68.226:8000/rank-list/?format=json";
-    ArrayList<Login_Class> employeeList = new ArrayList<>();
-    ArrayList<Rank_Class> rankList = new ArrayList<>();
 
-    public void putIntoLoginList(int id, String name, String password){
-        Login_Class newLogin = new Login_Class(id, name, password);
+    private String employee_url = "http://5.186.68.226:8000/employeegoodluckguessingthis9-list9/?format=json";
+    private String rank_url = "http://5.186.68.226:8000/rank-list/?format=json";
+    ArrayList<Employee_Class> employeeList = new ArrayList<>();
+    ArrayList<Rank_Class> rankList = new ArrayList<>();
+    RequestQueue requestQueue;
+
+
+
+
+    public void putIntoLoginList(int id, String name, String password, int rank){
+        Employee_Class newLogin = new Employee_Class(id, name, password, rank);
         employeeList.add(newLogin);
+    }
+
+
+    private void getEmployees(){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, employee_url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONArray jsonArray = response;
+                try {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        int id = jsonObject.getInt("id");
+                        String name = jsonObject.getString("name");
+                        String password = jsonObject.getString("password");
+                        int rank = jsonObject.getInt("rankId");
+                        putIntoLoginList(id, name, password, rank);
+                    }
+                } catch (Exception w) {
+                    Toast.makeText(Login.this, w.getMessage(), Toast.LENGTH_LONG);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Login.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        requestQueue = Volley.newRequestQueue(this);
+        EditText UserName = (EditText) findViewById(R.id.Login_UserInp);
+        EditText Password = (EditText) findViewById(R.id.LoginPassInp);
+        UserName.getText().clear();
+        Password.getText().clear();
+        getEmployees();
     }
 
-    public void login(View view){
-        Intent intent = new Intent(Login.this, Login_Main.class);
-        startActivity(intent);
+    public void authentication(View view){
+
+        EditText UserName = (EditText) findViewById(R.id.Login_UserInp);
+        EditText Password = (EditText) findViewById(R.id.LoginPassInp);
+        boolean validator = false;
+
+        String usernameInput = UserName.getText().toString();
+
+        String passwordInput = Password.getText().toString();
+
+        for (int i = 0; i < employeeList.size(); i++){
+            if (employeeList.get(i).name.matches(usernameInput) && employeeList.get(i).password.matches(passwordInput)){
+                validator = true;
+            }
+
+        }
+        if (validator == true){
+            Intent intent = new Intent(Login.this, Login_Main.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "Enter Correct UserName And Password", Toast.LENGTH_SHORT).show();
+        }
+
     }
+
+
+
+
+
 }
