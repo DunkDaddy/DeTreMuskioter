6c43f8611262bddf454eedd759ccf5258a62f268
DunkDaddy <59820523+DunkDaddy@users.noreply.github.com>
2022-06-08 13:01:08 +0200

    Stuff that need to be fixed. a lot
    I dont know what to do

--- app/src/main/java/com/example/cafevesuviusapp/MainActivity.java
+++ app/src/main/java/com/example/cafevesuviusapp/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, Login.class);
         startActivity(intent);
     }
+
+    public void test(View view){
+        Intent intent = new Intent(MainActivity.this, TestingCustomer.class);
+        startActivity(intent);
+    }
 }
