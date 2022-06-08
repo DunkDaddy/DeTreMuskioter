9b54849e5e6a11710f4a282e8f592b536dc556e2
Undying-Merch <jonasnikolailj@gmail.com>
2022-06-08 08:26:28 +0200

    Added Bar branch and Activity
    Added the bar branch and activity to the project.

---
+++ app/src/main/java/com/example/cafevesuviusapp/Classes/BarOrders.java
@@ -0,0 +1,14 @@
+package com.example.cafevesuviusapp.Classes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BarOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_orders);
+    }
+}
